146b7d391d9e45a0940a00cf401662f3ac5c415a
KnightenCooper <coo18028@byui.edu>
2021-09-14 13:46:57 -0500

    Added password hint that uses localStorage

---
+++ week1/js/week1.js
@@ -0,0 +1,14 @@
+function loadHint(){
+    var HintName = document.getElementById("name_input").value
+    var HintHTML = localStorage.getItem(HintName)
+    document.getElementById("Hint_editor").value = HintHTML
+    }
+function saveHint(){
+    var HintName = document.getElementById("name_input").value
+    var HintHTML = document.getElementById("Hint_editor").value
+    localStorage.setItem(HintName, HintHTML)
+    }
+function displayHint(){
+    var HintHTML = document.getElementById("Hint_editor").value
+    document.getElementById("Hint_display").innerHTML = HintHTML
+    }
