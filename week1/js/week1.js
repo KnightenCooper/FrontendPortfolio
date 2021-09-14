4e966195f499feb7113100725b4139dbd4c08bac
KnightenCooper <coo18028@byui.edu>
2021-09-14 17:04:44 -0500

    Added mobile UX notes and examples

--- week1/js/week1.js
+++ week1/js/week1.js
@@ -1,3 +1,5 @@
+
+// Javascript for localStorage page
 function loadHint(){
     var HintName = document.getElementById("username_input").value + "hint"
     var HintHTML = localStorage.getItem(HintName)
@@ -12,3 +14,16 @@ function displayHint(){
     var HintHTML = document.getElementById("Hint_editor").value
     document.getElementById("Hint_display").innerHTML = HintHTML
     }
+// Code source: https://masteringjs.io/tutorials/fundamentals/string-concat
+// https://www.w3.org/TR/WebCryptoAPI/#dfn-SubtleCrypto-method-digest
+
+
+// Javascript for mobile UX page
+    function myFunction() {
+        var x = document.getElementById("myLinks");
+        if (x.style.display === "block") {
+          x.style.display = "none";
+        } else {
+          x.style.display = "block";
+        }
+      }
