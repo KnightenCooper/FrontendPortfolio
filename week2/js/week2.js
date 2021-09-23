74cbbaa4acd3a37a729334e2a5c223a715f216f7
KnightenCooper <coo18028@byui.edu>
2021-09-23 07:46:52 -0500

    updated wk2 pages and added js for ch2 page

---
+++ week2/js/week2.js
@@ -0,0 +1,16 @@
+
+// Javascript for ch2 page
+const question = "What is Superman's real name?"
+const answer = prompt(question);
+alert(`You answered ${answer}`);
+
+
+// Javascript for mobile UX page
+    function myFunction() {
+        var x = document.getElementById("myLinks");
+        if (x.style.display === "block") {
+          x.style.display = "none";
+        } else {
+          x.style.display = "block";
+        }
+      }
