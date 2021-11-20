fd79bebb59147a649f30b1f36a75aed3bdcf8b30
KnightenCooper <coo18028@byui.edu>
2021-11-20 14:24:14 -0600

    updated fetch page to fix bugs

--- week10/js/fetch.js
+++ week10/js/fetch.js
@@ -1,59 +1,14 @@
-fetch('https://example.com/movies.json')
+fetch('https://jsonplaceholder.typicode.com/todos/1')
   .then(response => response.json())
   .then(data => console.log(data));
 
-  // Example POST method implementation:
-async function postData(url = '', data = {}) {
-  // Default options are marked with *
-  const response = await fetch(url, {
-    method: 'POST', // *GET, POST, PUT, DELETE, etc.
-    mode: 'cors', // no-cors, *cors, same-origin
-    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: 'same-origin', // include, *same-origin, omit
-    headers: {
-      'Content-Type': 'application/json'
-      // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    redirect: 'follow', // manual, *follow, error
-    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-    body: JSON.stringify(data) // body data type must match "Content-Type" header
-  });
-  return response.json(); // parses JSON response into native JavaScript objects
-}
-
-postData('https://example.com/answer', { answer: 42 })
-  .then(data => {
-    console.log(data); // JSON data parsed by `data.json()` call
-  });
-
-  fetch('https://example.com', {
-  credentials: 'include'
-});
-
-const data = { username: 'example' };
-
-fetch('https://example.com/profile', {
-  method: 'POST', // or 'PUT'
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify(data),
-})
-.then(response => response.json())
-.then(data => {
-  console.log('Success:', data);
-})
-.catch((error) => {
-  console.error('Error:', error);
-});
-
-const formData = new FormData();
+  const formData = new FormData();
 const fileField = document.querySelector('input[type="file"]');
 
 formData.append('username', 'abc123');
 formData.append('avatar', fileField.files[0]);
 
-fetch('https://example.com/profile/avatar', {
+fetch('https://jsonplaceholder.typicode.com/todos/1', {
   method: 'PUT',
   body: formData
 })
@@ -65,56 +20,23 @@ fetch('https://example.com/profile/avatar', {
   console.error('Error:', error);
 });
 
-async function* makeTextFileLineIterator(fileURL) {
-  const utf8Decoder = new TextDecoder('utf-8');
-  const response = await fetch(fileURL);
-  const reader = response.body.getReader();
-  let { value: chunk, done: readerDone } = await reader.read();
-  chunk = chunk ? utf8Decoder.decode(chunk) : '';
 
-  const re = /\n|\r|\r\n/gm;
-  let startIndex = 0;
-  let result;
+const formDataMultiple = new FormData();
+const photos = document.querySelector('input[type="file"][multiple]');
 
-  for (;;) {
-    let result = re.exec(chunk);
-    if (!result) {
-      if (readerDone) {
-        break;
-      }
-      let remainder = chunk.substr(startIndex);
-      ({ value: chunk, done: readerDone } = await reader.read());
-      chunk = remainder + (chunk ? utf8Decoder.decode(chunk) : '');
-      startIndex = re.lastIndex = 0;
-      continue;
-    }
-    yield chunk.substring(startIndex, result.index);
-    startIndex = re.lastIndex;
-  }
-  if (startIndex < chunk.length) {
-    // last line didn't end in a newline char
-    yield chunk.substr(startIndex);
-  }
+formDataMultiple.append('title', 'My Vegas Vacation');
+for (let i = 0; i < photos.files.length; i++) {
+  formDataMultiple.append(`photos_${i}`, photos.files[i]);
 }
 
-async function run() {
-  for await (let line of makeTextFileLineIterator(urlOfFile)) {
-    processLine(line);
-  }
-}
-
-run();
-
-fetch('flowers.jpg')
-  .then(response => {
-    if (!response.ok) {
-      throw new Error('Network response was not OK');
-    }
-    return response.blob();
-  })
-  .then(myBlob => {
-    myImage.src = URL.createObjectURL(myBlob);
-  })
-  .catch(error => {
-    console.error('There has been a problem with your fetch operation:', error);
-  });
+fetch('https://jsonplaceholder.typicode.com/todos/1', {
+  method: 'POST',
+  body: formDataMultiple,
+})
+.then(response => response.json())
+.then(result => {
+  console.log('Success:', result);
+})
+.catch(error => {
+  console.error('Error:', error);
+});
